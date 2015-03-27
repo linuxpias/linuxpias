594d9a09a7f9051431afde113768efbca6d233c7
rickyrish <ricardo.aranareyes@gmail.com>
2015-03-27 01:02:00 -0500

    creacion del app atriculos(incompleto)

---
+++ articulos/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class ArticuloTipo(models.Model):
+
+	nombre = models.CharField(max_length=50)
+	class Meta:
+		verbose_name = "Tipo de articulo"
+		verbose_name_plural = "Tipos de articulo"
+
+	def __str__(self):
+		return self.nombre
+
+class Etiqueta(models.Model):
+	nombre = models.CharField(max_length=50)
+
+	class Meta:
+		verbose_name = "Etiqueta"
+		verbose_name_plural = "Etiquetas"
+
+	def __str__(self):
+		return self.nombre
+    
+    
